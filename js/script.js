d6ce6df444c8e459677bbff5375d39099994331f
ecspetra <ecspetra@gmail.com>
2021-04-18 01:13:49 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -18,8 +18,45 @@ $(document).ready(function () {
   $(".news-slider").slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 2,
+    slidesToScroll: 3,
     prevArrow,
     nextArrow,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger, .navigation-menu").toggleClass("active");
+    $("body").toggleClass("lock");
   });
 });
