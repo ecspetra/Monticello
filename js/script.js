d4d01a1fa28a766fdf3b18ea7f6841bc5fd667ae
ecspetra <ecspetra@gmail.com>
2021-04-20 13:55:57 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -5,10 +5,38 @@ $(document).ready(function () {
   const nextArrow = `<div class="arrow-next"></div>`;
   $(".slider").slick({
     infinite: true,
+    vertical: true,
+    verticalSwiping: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    dotsClass: "hero-dots",
     prevArrow,
     nextArrow,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          vertical: false,
+          verticalSwiping: false,
+        },
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          vertical: false,
+          verticalSwiping: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          vertical: false,
+          verticalSwiping: false,
+        },
+      },
+    ],
   });
 });
 
@@ -19,6 +47,8 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    dots: true,
+    dotsClass: "news-dots",
     prevArrow,
     nextArrow,
     responsive: [
@@ -27,6 +57,7 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
+          arrows: false,
         },
       },
       {
@@ -34,6 +65,7 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
+          arrows: false,
         },
       },
       {
@@ -41,6 +73,7 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          arrows: false,
         },
       },
       {
@@ -48,6 +81,7 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          arrows: false,
         },
       },
     ],
